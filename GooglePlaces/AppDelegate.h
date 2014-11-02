0a5e7071007f67f39f2377683ed59bb921fa9afb
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-02 12:39:37 -0800

    Initial Commit

---
+++ GooglePlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GooglePlaces
+//
+//  Created by Anthony Dagati on 11/2/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GooglePlaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GooglePlaces
+//
+//  Created by Anthony Dagati on 11/2/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
